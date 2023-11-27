bff5b4e173d64ea00bc82642af2d64f9014ef217
matias <mandatariojmzarza@yahoo.com>
2023-11-27 14:06:59 -0300

    trabajo final

---
+++ estilos/login.js
@@ -0,0 +1,20 @@
+
+let .emaail= document.getElementById(email);
+let .passsword= password;
+
+let .passsword="123"
+
+console. log(email)
+console. log(passsword)
+
+let .administrador={
+    
+    user: admin@gmail.com
+    pass: 123 
+}
+
+console .log (el usuario es {administrador.email} y la contraseña es {administrador.passsword} 
+
+
+
+
