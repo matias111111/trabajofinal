d666ea03fa6ec53f3b64a6a48b7962528ce27234
matias <mandatariojmzarza@yahoo.com>
2023-12-05 10:31:00 -0300

    arreglos

--- js/login.js
+++ js/login.js
@@ -1,55 +1,30 @@
 
 
-let user = document.getElementById("email").value;
-let pass= document.getElementById("pass").value;
-
-
-console.log("email");
-console.log("pass");
-
-let administrador= {
-
-user:"matias@gmail.com",
-password: 1234,
-}
-
-console.log("el usuario es {administrador.email} y el pass es {administrador.pass}")
+let email= document.getElementById("email").value;
+let pass= document.getElementById("password").value;
 
 
 
+function login() {
 
-
-function login()  {
-var user,pass;
-
-     user = document.getElementById("email").Value;
-     pass = document.getElementById("pass").Value;
-
-    console.log("el user es {administrador.email} y la pass es {administrador.pass}")
-
-
-   if (user=="matias@gmail.com" && pass=="1234") {
-         alert("por favor ingrese un  usuario y una contrasena")
+ 
+   if (administrador.user=="matias@gmail.com" && administrador.password=="1234") {
         
-        windows.location= "../pages/contactosceo.html"   
+    location.href="../pages/administrador.html"
+
+        
     } 
     
 
 
 
-        if ("administrador.user=user && administrador.pass=password") {
+        if ("administrador.user==user && administrador.pass==password") {
            
-            wndows.location.href="//pages/administrador.html";
+            location.href="../pages/error.html";
         }else {
             alert("usuario y contraseña incorrecta");
         }
 
 
-
-        let imputUser = document.getElementById("email");
-        imputUser.value = "";
-
-        imputPass.document.getElementById("password");
-        imputPass.value = "";
 
     }
